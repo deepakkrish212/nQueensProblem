20440d58d6280d2d0975bb277a245cd8daf7530a
abemelech <abemelech.mesfin@gmail.com>
2022-10-28 09:29:45 -0500

    change visuals

--- nQueens.java
+++ nQueens.java
@@ -73,7 +73,11 @@ public class nQueens {
         public void printBoard() {
             for (int i = 0; i < board.length; i++) {
                 for (int j = 0; j < board[i].length; j++) {
-                    System.out.print(board[i][j] + " ");
+                    if (board[i][j] == 0) {
+                        System.out.print("- ");
+                    } else {
+                        System.out.print("Q ");
+                    }
                 }
                 System.out.println();
             }
